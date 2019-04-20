5bb9785a4cb0670f28839071be4a3385a880ecae
Alexandre Peyroux <alex@px.io>
2019-04-20 12:50:34 +0200

    add optargs

--- src/Main.hs
+++ src/Main.hs
@@ -8,11 +8,12 @@ import           Data.Attoparsec.Text
 import           Data.Scientific (coefficient
                                  , toRealFloat)
 import qualified Data.Text as T
+import qualified Options.Applicative as OA
 import           Shelly
 
 default (T.Text)
 
-data Action = Up | Down deriving Show
+data Action = Up | Down deriving (Show, Read, Eq)
 
 data Opt = Opt {
   optScreenName :: String
@@ -32,6 +33,18 @@ instance Bounded Brightness where
   minBound = Brightness 0.1
   maxBound = Brightness 1.0
 
+pOpt :: OA.Parser Opt
+pOpt = Opt
+  <$> OA.strOption
+    (OA.long "monitor"
+     <> OA.short 'm'
+     <> OA.metavar "MONITORNAME")
+  <*> OA.option OA.auto
+    (OA.long "action"
+     <> OA.short 'a'
+     <> OA.value Up
+     <> OA.metavar "ACTION")-- (pure Up OA.<|> pure Down)
+
 pMonitor :: T.Text -> Parser Monitor
 pMonitor name = Monitor
   <$> pure name
@@ -50,7 +63,7 @@ upBrightness m = shelly $ silently $ run_ "xrandr" ["--output"
                                                    , T.pack $ show (mBrightness m + 0.1)]
 
 downBrightness :: Monitor -> IO ()
-downBrightness Monitor { mBrightness = 0.1 } = return ()
+downBrightness Monitor { mBrightness = 0.3 } = return ()
 downBrightness m = shelly $ silently $ run_ "xrandr" ["--output"
                                                    , mName m
                                                    , "--brightness"
@@ -58,5 +71,15 @@ downBrightness m = shelly $ silently $ run_ "xrandr" ["--output"
 
 main :: IO ()
 main = do
-  putStrLn "hbrightness"
-  putStrLn "plop"
+  -- todo faire un check si xrandr est installé
+  args <- OA.execParser (OA.info pOpt OA.fullDesc)
+  xr <- xrandr
+  case args of
+    Opt { optAction = Up } -> 
+      case maybeResult $ parse (pMonitor "eDP-1") xr of
+        Just m -> upBrightness m
+        Nothing -> putStrLn "Monitor not found"
+    Opt { optAction = Down } -> 
+      case maybeResult $ parse (pMonitor "eDP-1") xr of
+        Just m -> downBrightness m
+        Nothing -> putStrLn "Monitor not found"
